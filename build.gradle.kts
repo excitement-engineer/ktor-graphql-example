a43fdc97b092f06c84bd6a0007bfd1372a136592
Dirk-Jan Rutten <dirkjanrutten@gmail.com>
2021-02-26 13:33:35 +0100

    Updated to newest version

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,22 +1,20 @@
 plugins {
-    kotlin("jvm") version "1.3.72"
+    kotlin("jvm") version "1.4.30"
 }
 
 
 repositories {
     mavenCentral()
-    jcenter()
 }
 
-val ktor_version = "1.3.2"
+val ktor_version = "1.5.1"
 
 dependencies {
-    compile(kotlin("stdlib-jdk8"))
     compile("io.ktor:ktor-server-netty:$ktor_version")
 
     compile("com.graphql-java:graphql-java:14.0")
     compile("org.apache.logging.log4j:log4j-slf4j-impl:2.9.1")
-    compile("com.github.excitement-engineer:ktor-graphql:2.0.2")
+    compile("com.github.excitement-engineer:ktor-graphql:2.1.0")
 
     testImplementation("com.google.code.gson:gson:2.8.0")
     testImplementation("junit:junit:4.11")
